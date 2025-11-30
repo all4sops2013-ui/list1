e377dc5880204ff3e5682e98684a509453318e0a
all4sops2013-ui <all4sops2013@gmail.com>
2025-11-30 13:24:43 +0000

    Add files via upload

---
+++ data structure.py
@@ -0,0 +1,21 @@
+# data structure is used to store multiple data in a list
+# list 
+list1 = [1,2,3,4,5,6]
+list2 = [20,30,90,100,200]
+list3 = ["hello,False,78.9"]
+
+print(list1)
+print(list2)
+print(list3)
+
+print(list1[2])
+print(list1[2:5])
+
+print(len(list1))
+print(max(list1))
+print(min(list1))
+
+print("------------------------ ")
+print(list2)
+list2.reverse()
+print(list2)
